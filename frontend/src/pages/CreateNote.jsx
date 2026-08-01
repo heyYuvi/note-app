ed896c07dc712dd8457c5dafad2a0818ee51fcc2
heyYuvi <prasadyuvraj39@gmail.com>
2026-08-01 20:03:27 +0530

    Implement styling for create and edit note pages

--- frontend/src/pages/CreateNote.jsx
+++ frontend/src/pages/CreateNote.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react"
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
+import { FaFile } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa";
 
-const CreateNote = () =>{
+
+
+const CreateNote = () => {
 
     const navigate = useNavigate();
 
@@ -11,37 +15,65 @@ const CreateNote = () =>{
         description: ""
     });
 
-    const handleChange = (e) =>{
+    const handleChange = (e) => {
         setForm({
             ...form, [e.target.name]: e.target.value
         });
     }
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
-            
-        await api.post("/note", form);
-        setForm({
-            title: "",
-            description: ""
-        });
+        try {
 
-        navigate("/");
-        }catch(error){
+            await api.post("/note", form);
+            setForm({
+                title: "",
+                description: ""
+            });
+
+            navigate("/");
+        } catch (error) {
             console.log(error.response?.data?.message || error.message);
         }
     }
 
     return (
-        <>
-        <form onSubmit={handleSubmit}>
-            Title <input type="text" name="title" value={form.title} onChange={handleChange} ></input>
-            <textarea name="description" value={form.description}  onChange={handleChange} placeholder="Description"></textarea>
-            <button type="submit">Create Note</button>
-        </form>
-        </>
+        <div className="h-full w-full">
+            <div className="max-w-6xl m-auto w-full p-8">
+                <div className="flex flex-col">
+                    <div>
+                        <p className="font-semibold text-[#3525CD]">---- NEW WORKSPACE</p>
+                        <h2 className="font-bold text-xl">Create New Note</h2>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex justify-center">
+                            <div className=" mt-10 w-4xl h-140 shadow-2xl p-8">
+                        <div className="flex flex-col gap-4">
+                            <input type="text" name="title" value={form.title} onChange={handleChange} placeholder="Enter title..." className="text-4xl text-blue-500 focus:outline-none focus:ring-0"></input>
+                            <div className="flex gap-6 mb-10">
+                                <p className="bg-blue-200 px-2 rounded-md font-semibold text-white flex items-center gap-1"><FaFile />Personal</p>
+                                <p className="text-gray-400">{new Date().toLocaleDateString("en-UN", {
+                                    day: "numeric",
+                                    month: "short",
+                                    year: "numeric"
+                                })}</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col">
+                        <textarea name="description" value={form.description} onChange={handleChange} placeholder="Description" placeholder="Start typing your thoughts..." className="w-3xl h-80 resize-none focus:outline-none focus:ring-0"></textarea>
+                       <div className="flex justify-end-safe gap-6">
+                        <button onClick={() =>{navigate("/")}} className="font- text-gray-500 cursor-pointer">Cancel</button>
+                         <button type="submit" className="flex gap-2 px-4 py-2 bg-blue-600 text-white hover:bg-blue-800 rounded-md cursor-pointer transition">Create Note <FaArrowRight /></button>
+                       </div>
+                        </div>
+                    
+                        </div>
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/EditNote.jsx
+++ frontend/src/pages/EditNote.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../services/api";
+import { RiEditBoxFill } from "react-icons/ri";
+import { formatDistanceToNow } from "date-fns";
+import { CiCircleCheck } from "react-icons/ci";
+import { GoArrowLeft } from "react-icons/go";
 
-const EditNote = () =>{
+
+
+
+const EditNote = () => {
     const { id } = useParams();
 
     const navigate = useNavigate();
@@ -12,54 +19,92 @@ const EditNote = () =>{
         description: ""
     });
 
-    useEffect(() =>{
-        const fetchNote = async () =>{
-            try{
-                 const res = await api.get(`/note/${id}`);
-                 setForm({
+    const [update, setUpdate] = useState(null);
+
+    useEffect(() => {
+        const fetchNote = async () => {
+            try {
+                const res = await api.get(`/note/${id}`);
+                console.log(res);
+                setForm({
                     title: res.data.data.title,
                     description: res.data.data.description
-                 });
-            
-            }catch(error){
+                });
+                setUpdate(res.data.data.updatedAt);
+
+            } catch (error) {
                 console.log(error.response?.data?.message || error.message);
-            } 
+            }
         }
 
         fetchNote();
 
     }, [id]);
 
-    const handleChange = (e) =>{
+
+    const handleChange = (e) => {
         setForm({
             ...form, [e.target.name]: e.target.value
         });
     }
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
-            
-        await api.put(`/note/${id}`, form);
-        setForm({
-            title: "",
-            description: ""
-        });
-        navigate("/");
-        }catch(error){
+        try {
+
+            await api.put(`/note/${id}`, form);
+            setForm({
+                title: "",
+                description: ""
+            });
+            navigate("/");
+        } catch (error) {
             console.log(error.response?.data?.message || error.message);
         }
     }
 
     return (
-        <>
-        <form onSubmit={handleSubmit}>
-            Note Title <input type="text" name="title" value={form.title} onChange={handleChange}></input>
-            <textarea name="description" value={form.description} onChange={handleChange}></textarea>
-            <button>Save Changes</button>
-        </form>
-        </>
+        <div className="h-screen w-full">
+        <div className="max-w-6xl m-auto w-full p-10 ">
+            <div>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                    <RiEditBoxFill className="text-blue-700" /><p className="font-semibold">EDITOR MODE</p>
+                </div>
+                <div>
+                    
+                    <p className="text-gray-500">Last updated {formatDistanceToNow(new Date(update), {
+                        addSuffix: true,
+                    })}</p>
+                </div>
+                </div>
+                
+                    <form onSubmit={handleSubmit} className="flex justify-center">
+                        <div>
+                            <div className="shadow-2xl px-8 py-12 mt-16 w-4xl flex flex-col gap-10">
+                        <div className="flex flex-col gap-2">
+                            <h2 className="text-[#3525CD] font-semibold ">
+                                Note Title
+                            </h2>
+                        <input type="text" name="title" value={form.title} onChange={handleChange} className="bg-blue-100 px-4 py-2 rounded-md focus:outline-none focus:ring-0"></input>
+                        </div>    
+                         
+                        <div>
+                            <textarea name="description" value={form.description} onChange={handleChange} className="w-3xl h-80 resize-none focus:outline-none"></textarea>
+                        </div>
+                        </div>
+                        <div className="flex gap-6 mt-6 justify-end-safe">
+                            
+                        <button onClick={() =>{navigate("/")}} className="flex items-center gap-2"><GoArrowLeft /> Cancel</button>
+                        <button className="bg-blue-600 text-white font-bold px-4 py-2 rounded-md hover:bg-blue-800 cursor-pointer flex items-center gap-2"><CiCircleCheck size={20}/>Save Changes</button>
+                        </div>
+                        </div>
+                    </form>
+                
+            </div>
+        </div>
+        </div>
     )
 }
 
